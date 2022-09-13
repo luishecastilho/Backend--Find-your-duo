41341253da4e6b936d1b3ed49670a44b52d07507
luishecastilho <manocastilho07@gmail.com>
2022-09-12 21:30:57 -0300

    ambiente/fluxo NODE ta pronto

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+//const express = require('express');
+import express from 'express';
+
+const app = express()
+
+app.get("/ads", (request, response) => {
+    response.json([
+        {id: 1, name: 'anuncio 1'},
+        {id: 2, name: 'anuncio 2'},
+        {id: 3, name: 'anuncio 3'},
+        {id: 4, name: 'anuncio333'}
+    ]);
+})
+
+app.listen(3333)
